4802f2dbeeeca02156ce2469920600befe0fdd96
somasekimoto <0421.soma@gmail.com>
2024-01-14 17:13:38 +0900

    add: revert, receive method & Received event

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,15 +4,23 @@ pragma solidity >=0.8.0 <0.9.0;
 contract HelloWorld {
     string public data;
 
+    event Received(address sender, uint amount);
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+
     function Hello() public pure returns (string memory) {
         return "Hello World";
     }
 
     function Greet(string memory str) public pure returns (string memory) {
+        require(bytes(str).length > 0, "string must not be empty");
         return str;
     }
 
     function SetData(string memory str) public {
+        require(bytes(str).length > 0, "string must not be empty");
         data = str;
     }
 
--- gocode/hello_world.go
+++ gocode/hello_world.go
@@ -31,8 +31,8 @@ var (
 
 // HelloworldMetaData contains all meta data concerning the Helloworld contract.
 var HelloworldMetaData = &bind.MetaData{
-	ABI: "[{\"inputs\":[],\"name\":\"GetBalance\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"GetData\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"str\",\"type\":\"string\"}],\"name\":\"Greet\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"Hello\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"str\",\"type\":\"string\"}],\"name\":\"SetData\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"Withdraw\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"data\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
-	Bin: "0x608060405234801561000f575f80fd5b506104e18061001d5f395ff3fe608060405234801561000f575f80fd5b506004361061007a575f3560e01c8063b7e8260c11610058578063b7e8260c146100ae578063bcdfe0d5146100c1578063efdeaaf5146100e8578063f8f8a912146100f9575f80fd5b806357ea89b61461007e57806373d4a13a1461008857806376b8e528146100a6575b5f80fd5b610086610107565b005b610090610133565b60405161009d919061025c565b60405180910390f35b6100906101be565b6100866100bc3660046102bc565b61024d565b60408051808201909152600b81526a12195b1b1bc815dbdc9b1960aa1b6020820152610090565b6100906100f63660046102bc565b90565b60405147815260200161009d565b60405133904780156108fc02915f818181858888f19350505050158015610130573d5f803e3d5ffd5b50565b5f805461013f90610367565b80601f016020809104026020016040519081016040528092919081815260200182805461016b90610367565b80156101b65780601f1061018d576101008083540402835291602001916101b6565b820191905f5260205f20905b81548152906001019060200180831161019957829003601f168201915b505050505081565b60605f80546101cc90610367565b80601f01602080910402602001604051908101604052809291908181526020018280546101f890610367565b80156102435780601f1061021a57610100808354040283529160200191610243565b820191905f5260205f20905b81548152906001019060200180831161022657829003601f168201915b5050505050905090565b5f61025882826103eb565b5050565b5f602080835283518060208501525f5b818110156102885785810183015185820160400152820161026c565b505f604082860101526040601f19601f8301168501019250505092915050565b634e487b7160e01b5f52604160045260245ffd5b5f602082840312156102cc575f80fd5b813567ffffffffffffffff808211156102e3575f80fd5b818401915084601f8301126102f6575f80fd5b813581811115610308576103086102a8565b604051601f8201601f19908116603f01168101908382118183101715610330576103306102a8565b81604052828152876020848701011115610348575f80fd5b826020860160208301375f928101602001929092525095945050505050565b600181811c9082168061037b57607f821691505b60208210810361039957634e487b7160e01b5f52602260045260245ffd5b50919050565b601f8211156103e657805f5260205f20601f840160051c810160208510156103c45750805b601f840160051c820191505b818110156103e3575f81556001016103d0565b50505b505050565b815167ffffffffffffffff811115610405576104056102a8565b610419816104138454610367565b8461039f565b602080601f83116001811461044c575f84156104355750858301515b5f19600386901b1c1916600185901b1785556104a3565b5f85815260208120601f198616915b8281101561047a5788860151825594840194600190910190840161045b565b508582101561049757878501515f19600388901b60f8161c191681555b505060018460011b0185555b50505050505056fea26469706673582212203957cd4bf277b5a196201496304d3ddee001125df463cd39769ff5f5fb9943e064736f6c63430008170033",
+	ABI: "[{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"address\",\"name\":\"sender\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"Received\",\"type\":\"event\"},{\"inputs\":[],\"name\":\"GetBalance\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"GetData\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"str\",\"type\":\"string\"}],\"name\":\"Greet\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"Hello\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"pure\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"str\",\"type\":\"string\"}],\"name\":\"SetData\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"Withdraw\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"data\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"stateMutability\":\"payable\",\"type\":\"receive\"}]",
+	Bin: "0x608060405234801561000f575f80fd5b5061060e8061001d5f395ff3fe608060405260043610610071575f3560e01c8063b7e8260c1161004c578063b7e8260c14610108578063bcdfe0d514610127578063efdeaaf51461015a578063f8f8a91214610179575f80fd5b806357ea89b6146100b457806373d4a13a146100ca57806376b8e528146100f4575f80fd5b366100b057604080513381523460208201527f88a5966d370b9919b20f3e2c13ff65706f196a4e32cc2c12bf57088f88525874910160405180910390a1005b5f80fd5b3480156100bf575f80fd5b506100c8610193565b005b3480156100d5575f80fd5b506100de6101bf565b6040516100eb9190610389565b60405180910390f35b3480156100ff575f80fd5b506100de61024a565b348015610113575f80fd5b506100c86101223660046103e9565b6102d9565b348015610132575f80fd5b5060408051808201909152600b81526a12195b1b1bc815dbdc9b1960aa1b60208201526100de565b348015610165575f80fd5b506100de6101743660046103e9565b610338565b348015610184575f80fd5b506040514781526020016100eb565b60405133904780156108fc02915f818181858888f193505050501580156101bc573d5f803e3d5ffd5b50565b5f80546101cb90610494565b80601f01602080910402602001604051908101604052809291908181526020018280546101f790610494565b80156102425780601f1061021957610100808354040283529160200191610242565b820191905f5260205f20905b81548152906001019060200180831161022557829003601f168201915b505050505081565b60605f805461025890610494565b80601f016020809104026020016040519081016040528092919081815260200182805461028490610494565b80156102cf5780601f106102a6576101008083540402835291602001916102cf565b820191905f5260205f20905b8154815290600101906020018083116102b257829003601f168201915b5050505050905090565b5f8151116103295760405162461bcd60e51b8152602060048201526018602482015277737472696e67206d757374206e6f7420626520656d70747960401b60448201526064015b60405180910390fd5b5f6103348282610518565b5050565b60605f8251116103855760405162461bcd60e51b8152602060048201526018602482015277737472696e67206d757374206e6f7420626520656d70747960401b6044820152606401610320565b5090565b5f602080835283518060208501525f5b818110156103b557858101830151858201604001528201610399565b505f604082860101526040601f19601f8301168501019250505092915050565b634e487b7160e01b5f52604160045260245ffd5b5f602082840312156103f9575f80fd5b813567ffffffffffffffff80821115610410575f80fd5b818401915084601f830112610423575f80fd5b813581811115610435576104356103d5565b604051601f8201601f19908116603f0116810190838211818310171561045d5761045d6103d5565b81604052828152876020848701011115610475575f80fd5b826020860160208301375f928101602001929092525095945050505050565b600181811c908216806104a857607f821691505b6020821081036104c657634e487b7160e01b5f52602260045260245ffd5b50919050565b601f82111561051357805f5260205f20601f840160051c810160208510156104f15750805b601f840160051c820191505b81811015610510575f81556001016104fd565b50505b505050565b815167ffffffffffffffff811115610532576105326103d5565b610546816105408454610494565b846104cc565b602080601f831160018114610579575f84156105625750858301515b5f19600386901b1c1916600185901b1785556105d0565b5f85815260208120601f198616915b828110156105a757888601518255948401946001909101908401610588565b50858210156105c457878501515f19600388901b60f8161c191681555b505060018460011b0185555b50505050505056fea2646970667358221220562df10e020e80262899256f3382c5ab5d46ed4157aaf312d0e51cdd1d102ddf64736f6c63430008170033",
 }
 
 // HelloworldABI is the input ABI used to generate the binding from.
@@ -397,4 +397,160 @@ func (_Helloworld *HelloworldSession) Withdraw() (*types.Transaction, error) {
 // Solidity: function Withdraw() returns()
 func (_Helloworld *HelloworldTransactorSession) Withdraw() (*types.Transaction, error) {
 	return _Helloworld.Contract.Withdraw(&_Helloworld.TransactOpts)
+}
+
+// Receive is a paid mutator transaction binding the contract receive function.
+//
+// Solidity: receive() payable returns()
+func (_Helloworld *HelloworldTransactor) Receive(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Helloworld.contract.RawTransact(opts, nil) // calldata is disallowed for receive function
+}
+
+// Receive is a paid mutator transaction binding the contract receive function.
+//
+// Solidity: receive() payable returns()
+func (_Helloworld *HelloworldSession) Receive() (*types.Transaction, error) {
+	return _Helloworld.Contract.Receive(&_Helloworld.TransactOpts)
+}
+
+// Receive is a paid mutator transaction binding the contract receive function.
+//
+// Solidity: receive() payable returns()
+func (_Helloworld *HelloworldTransactorSession) Receive() (*types.Transaction, error) {
+	return _Helloworld.Contract.Receive(&_Helloworld.TransactOpts)
+}
+
+// HelloworldReceivedIterator is returned from FilterReceived and is used to iterate over the raw logs and unpacked data for Received events raised by the Helloworld contract.
+type HelloworldReceivedIterator struct {
+	Event *HelloworldReceived // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *HelloworldReceivedIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(HelloworldReceived)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(HelloworldReceived)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *HelloworldReceivedIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *HelloworldReceivedIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// HelloworldReceived represents a Received event raised by the Helloworld contract.
+type HelloworldReceived struct {
+	Sender common.Address
+	Amount *big.Int
+	Raw    types.Log // Blockchain specific contextual infos
+}
+
+// FilterReceived is a free log retrieval operation binding the contract event 0x88a5966d370b9919b20f3e2c13ff65706f196a4e32cc2c12bf57088f88525874.
+//
+// Solidity: event Received(address sender, uint256 amount)
+func (_Helloworld *HelloworldFilterer) FilterReceived(opts *bind.FilterOpts) (*HelloworldReceivedIterator, error) {
+
+	logs, sub, err := _Helloworld.contract.FilterLogs(opts, "Received")
+	if err != nil {
+		return nil, err
+	}
+	return &HelloworldReceivedIterator{contract: _Helloworld.contract, event: "Received", logs: logs, sub: sub}, nil
+}
+
+// WatchReceived is a free log subscription operation binding the contract event 0x88a5966d370b9919b20f3e2c13ff65706f196a4e32cc2c12bf57088f88525874.
+//
+// Solidity: event Received(address sender, uint256 amount)
+func (_Helloworld *HelloworldFilterer) WatchReceived(opts *bind.WatchOpts, sink chan<- *HelloworldReceived) (event.Subscription, error) {
+
+	logs, sub, err := _Helloworld.contract.WatchLogs(opts, "Received")
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(HelloworldReceived)
+				if err := _Helloworld.contract.UnpackLog(event, "Received", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseReceived is a log parse operation binding the contract event 0x88a5966d370b9919b20f3e2c13ff65706f196a4e32cc2c12bf57088f88525874.
+//
+// Solidity: event Received(address sender, uint256 amount)
+func (_Helloworld *HelloworldFilterer) ParseReceived(log types.Log) (*HelloworldReceived, error) {
+	event := new(HelloworldReceived)
+	if err := _Helloworld.contract.UnpackLog(event, "Received", log); err != nil {
+		return nil, err
+	}
+	event.Raw = log
+	return event, nil
 }
